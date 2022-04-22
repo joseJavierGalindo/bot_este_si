630ee19b22253b89e0fcbedd95d08e901b58a1bc
joseJavierGalindo <100740011+joseJavierGalindo@users.noreply.github.com>
2022-04-22 12:54:18 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
-BOT_TOKEN = ''
-API_ID = 
-API_HASH = ''
+BOT_TOKEN = '5253833673:AAEvJbDxfcN3NeYgvQT3UQ4-SLLSPmEW3Wo'
+API_ID = 16622951
+API_HASH = '21b795f8112a5546e9e32e7358e9d401'
 
 
 # DATOS DEL MOODLE
-USUARIO = ''
-PASSWORD = ''
-USUARIO_ID = 
-ZIP_MB = 350
-MOODLE_URL = 'moodle.uclv.edu.cu'
-OWNER = []
+USUARIO = 'darian.borges@estudiantes.fbio.uh.cu'
+PASSWORD = 'darian1995'
+USUARIO_ID = 1007430305
+ZIP_MB = 245
+MOODLE_URL = 'evea.uh.cu'
+OWNER = [Jotax2G]
 
 # CUENTA DE MEGA
 GMAIL_MEGA = 'yanco148@gmail.com'
