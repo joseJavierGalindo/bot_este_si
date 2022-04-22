4309e7aad3dc8182a0a75d38c81ecaf989e51078
joseJavierGalindo <100740011+joseJavierGalindo@users.noreply.github.com>
2022-04-22 12:44:50 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+BOT_TOKEN = ''
+API_ID = 
+API_HASH = ''
+
+
+# DATOS DEL MOODLE
+USUARIO = ''
+PASSWORD = ''
+USUARIO_ID = 
+ZIP_MB = 350
+MOODLE_URL = 'moodle.uclv.edu.cu'
+OWNER = []
+
+# CUENTA DE MEGA
+GMAIL_MEGA = 'yanco148@gmail.com'
+PASS_MEGA = 'yancarlos1482005'
